457c363c48f531f499ffe03b6a7a7b98c6125441
Christopher Stanley <chris@cbstanley.dev>
2021-11-21 15:24:01 -0700

    configure project file and folder structure

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  "stories": ['../src/components/**/*.stories.js'],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
